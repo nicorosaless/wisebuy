e0c9356aea02fad9b635de0051da0e12a35820e9
nicorosales <nirogo06@gmail.com>
2025-05-03 18:42:53 +0200

    update

--- startserver.py
+++ startserver.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("widget.scraping.hey:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("widget.backend.app:app", host="0.0.0.0", port=8000, reload=True)
